b11f7a4dd99c23f758b0671fc48b88a4a251090d
olelit <litasov00@gmail.com>
2025-10-31 11:02:03 +0300

    setup differend settings for multiple users

--- fill_redmine_dev_task.py
+++ fill_redmine_dev_task.py
@@ -8,13 +8,69 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-API_KEY = os.getenv("REDMINE_API_KEY")
 BASE_URL = os.getenv("REDMINE_BASE_URL")
-USER_ID = os.getenv("USER_ID")
-USSUE_ID = os.getenv("USSUE_ID")
-COMMENT = os.getenv("COMMENT")
-ACTIVITY_ID = os.getenv("ACTIVITY_ID")
 
+async def update_redmine_activity(uid: int, iid: int, comment: str, activity_id: int, api_key: str):
+    excludeEnv = os.getenv("EXCLUDE_DATES")
+    exclude = set()
+
+    if excludeEnv:
+        exclude = set(excludeEnv.split(","))
+
+    hours = 8
+    base_url = BASE_URL
+    url = f"{base_url}/time_entries.xml"
+    headers = {
+        "Content-Type": "application/xml",
+        "X-Redmine-API-Key": api_key
+    }
+
+    today = date.today()
+    first_day = today.replace(day=1)
+    if first_day.month == 12:
+        next_month = first_day.replace(year=first_day.year + 1, month=1, day=1)
+    else:
+        next_month = first_day.replace(month=first_day.month + 1, day=1)
+    last_day = next_month - timedelta(days=1)
+
+    spent_on_dates = get_times(base_url, headers, iid, uid)
+
+    affected_dates = []
+    current_day = first_day
+    while current_day <= last_day:
+        day_str = current_day.isoformat()
+        if (
+            current_day.weekday() < 5
+            and day_str not in exclude
+            and day_str not in spent_on_dates
+        ):
+            affected_dates.append(day_str)
+        current_day += timedelta(days=1)
+
+    print("The following dates will be affected:")
+    for date_str in affected_dates:
+        print(f"- {date_str}")
+    print(f"Hours: {hours}")
+    print(f"Comment: {comment}")
+
+    proceed = input("Do you want to continue? (y/n): ").strip().lower()
+    if proceed != "y":
+        print("Script execution stopped by the user.")
+        return
+
+    async with aiohttp.ClientSession() as session:
+        current_day = first_day
+        tasks = []
+        while current_day <= last_day:
+            day_str = current_day.isoformat()
+            if (
+                current_day.weekday() < 5
+                and day_str not in exclude
+                and day_str not in spent_on_dates
+            ):
+                tasks.append(set_time(session, url, headers, iid, uid, current_day, hours, activity_id, comment))
+            current_day += timedelta(days=1)
+        await asyncio.gather(*tasks)
 
 async def set_time(session, url, headers, iid, uid, day, hours, activity_id, comments):
     body = f"""
@@ -58,79 +114,20 @@ def get_times(base_url, headers, iid, uid):
     return spent_on_dates
 
 async def main():
-    iid = USSUE_ID
-    uid = USER_ID
-    exclude = set()
-    hours = 8
-    activity_id = ACTIVITY_ID
-    comments = COMMENT
-    if len(sys.argv) > 3:
-        exclude = set(d.strip() for d in sys.argv[3].split(",") if d.strip())
-    if len(sys.argv) > 4:
-        hours = sys.argv[4]
-    if len(sys.argv) > 5:
-        activity_id = sys.argv[5]
-    if len(sys.argv) > 6:
-        comments = sys.argv[6]
+    i: int = 1
+    while True:
+        iid = os.getenv(f"USSUE_ID_{i}")
 
-    base_url = BASE_URL
-    url = f"{base_url}/time_entries.xml"
-    headers = {
-        "Content-Type": "application/xml",
-        "X-Redmine-API-Key": API_KEY
-    }
+        if iid is None:
+            break
 
-    today = date.today()
-    first_day = today.replace(day=1)
-    if first_day.month == 12:
-        next_month = first_day.replace(year=first_day.year + 1, month=1, day=1)
-    else:
-        next_month = first_day.replace(month=first_day.month + 1, day=1)
-    last_day = next_month - timedelta(days=1)
+        uid = os.getenv(f"USER_ID_{i}")
+        comment = os.getenv(f"COMMENT_{i}")
+        activity_id = os.getenv(f"ACTIVITY_ID_{i}")
+        api_key = os.getenv(f"REDMINE_API_KEY_{i}")
+        update_redmine_activity(uid, iid, comment, activity_id, api_key)
 
-    spent_on_dates = get_times(base_url, headers, iid, uid)
 
-    print(spent_on_dates)
-
-    # Calculate affected dates
-    affected_dates = []
-    current_day = first_day
-    while current_day <= last_day:
-        day_str = current_day.isoformat()
-        if (
-            current_day.weekday() < 5
-            and day_str not in exclude
-            and day_str not in spent_on_dates
-        ):
-            affected_dates.append(day_str)
-        current_day += timedelta(days=1)
-
-    # Display information about affected dates
-    print("The following dates will be affected:")
-    for date_str in affected_dates:
-        print(f"- {date_str}")
-    print(f"Hours: {hours}")
-    print(f"Comment: {comments}")
-
-    # Ask for user confirmation
-    proceed = input("Do you want to continue? (y/n): ").strip().lower()
-    if proceed != "y":
-        print("Script execution stopped by the user.")
-        return
-
-    async with aiohttp.ClientSession() as session:
-        current_day = first_day
-        tasks = []
-        while current_day <= last_day:
-            day_str = current_day.isoformat()
-            if (
-                current_day.weekday() < 5
-                and day_str not in exclude
-                and day_str not in spent_on_dates
-            ):
-                tasks.append(set_time(session, url, headers, iid, uid, current_day, hours, activity_id, comments))
-            current_day += timedelta(days=1)
-        await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
